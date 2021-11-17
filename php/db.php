cd01a85186dcbac1c4d395db217548c6b79cf76b
BrianOrihuelaP <89498777+BrianOrihuelaP@users.noreply.github.com>
2021-11-16 19:52:18 -0600

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,14 +1,16 @@
 <?php
-
-$server = 'localhost:3306';
-$username = 'root';
-$password = '';
-$database = 'TransmisionDatos';
-
+// PHP Data Objects(PDO) Sample Code:
 try {
-  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
-} catch (PDOException $e) {
-  die('Connection Failed: ' . $e->getMessage());
+    $conn = new PDO("sqlsrv:server = tcp:proyectofab.database.windows.net,1433; Database = usuariosFab", "brian", "{Ties15059922}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "brian", "pwd" => "{Ties15059922}", "Database" => "usuariosFab", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:proyectofab.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
